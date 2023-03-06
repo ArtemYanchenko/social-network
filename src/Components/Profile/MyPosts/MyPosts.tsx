7a819f7036702c41f9a8d820f832303752cbfbd0
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-06 16:13:27 +0300

    edit in 15 lesson

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
+import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
         <>
+            <textarea></textarea>
+            <button>add post</button>
             <div>my posts</div>
-            <div>post1</div>
-            <div>post2</div>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
         </>
 
     );
--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -3,8 +3,10 @@ import classes from './Post.module.css';
 
 const Post = () => {
     return (
-        <div className={classes.content}>
-            post
+        <div className={classes.item}>
+
+            <img src="https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1678108303~exp=1678108903~hmac=7aa8cde4054338765d1f742abe6d6352da726ad344ad4649ceb2042dbf007dde" alt="" className={classes.avatar}/>
+            <p>post 1</p>
         </div>
     );
 };
