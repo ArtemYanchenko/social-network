6caaae7b7c0b545cbabb608c73d81767c41818d8
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-06 15:02:47 +0300

    edit .css files

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import classes from './Header.module.css';
+
 
 const  Header = () => {
     return (
-        <header className="header">
+        <header className={classes.header}>
             <img src="https://cdn-icons-png.flaticon.com/128/124/124019.png" alt="logo"/>
         </header>
     );
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={classes.content}>
             <div><img
                 src="https://img3.akspic.ru/crops/7/3/3/7/4/147337/147337-macbook-makbuk_ejr-voda-tkan-more-3840x2160.jpg"
                 alt="photo"/>
