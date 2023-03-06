1cfac0267ee7ac798150cef5f34c75a70829d945
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-06 14:12:17 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <div className="app-wrapper">
+                <Header />
+                <Navbar />
+                <Profile />
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img
+                src="https://img3.akspic.ru/crops/7/3/3/7/4/147337/147337-macbook-makbuk_ejr-voda-tkan-more-3840x2160.jpg"
+                alt="photo"/>
+            </div>
+            <div>ava + desc</div>
+            <div>my posts</div>
+            <div>new post</div>
+            CONTENT
+        </div>
+    );
+};
+
+export default Profile;
