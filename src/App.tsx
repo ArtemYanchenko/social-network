d9006464ce4508df120c1757d7b3dafd0c66bd3b
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-07 15:21:32 +0300

    done in 18 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
 
 
 
@@ -12,7 +13,11 @@ function App() {
             <div className="app-wrapper">
                 <Header />
                 <Navbar />
-                <Profile />
+                <div className="app-wrapper-content">
+                    {/*<Profile />*/}
+                    <Dialogs/>
+                </div>
+
             </div>
         </div>
     );
