4e0cf271b6fde5c89245741e523d60db6d000308
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-07 12:14:36 +0300

    done in 16 lesson

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -5,13 +5,12 @@ import Post from './Post/Post';
 const MyPosts = () => {
     return (
         <>
+            <div>my posts</div>
             <textarea></textarea>
             <button>add post</button>
-            <div>my posts</div>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
+            <Post message="hi, my first post" likeCount={5}/>
+            <Post message="i am fine" likeCount={10}/>
+
         </>
 
     );
--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import classes from './Post.module.css';
 
-const Post = () => {
+export type PostPropsType = {
+    message: string,
+    likeCount:number
+}
+
+const Post = (props: PostPropsType) => {
     return (
         <div className={classes.item}>
 
             <img src="https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1678108303~exp=1678108903~hmac=7aa8cde4054338765d1f742abe6d6352da726ad344ad4649ceb2042dbf007dde" alt="" className={classes.avatar}/>
-            <p>post 1</p>
+            <p>{props.message}</p>
+
+            <p>- like: {props.likeCount}</p>
         </div>
     );
 };
