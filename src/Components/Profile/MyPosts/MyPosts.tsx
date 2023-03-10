2526fcbd9d6f5e6385cfcb3bae567a46ed4ca61a
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-10 11:33:24 +0300

    edit in 23 lesson

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -4,14 +4,13 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
-        <>
-            <div>my posts</div>
+        <div className={classes.postsBlock}>
+            <h3> my posts </h3>
             <textarea></textarea>
             <button>add post</button>
             <Post message="hi, my first post" likeCount={5}/>
             <Post message="i am fine" likeCount={10}/>
-
-        </>
+        </div>
 
     );
 };
--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -9,7 +9,7 @@ const ProfileInfo = () => {
                 className={classes.mainImg}
                 src="https://img3.akspic.ru/crops/7/3/3/7/4/147337/147337-macbook-makbuk_ejr-voda-tkan-more-3840x2160.jpg"
                 alt="photo"/>
-            <div>ava+desc</div>
+            <div className={classes.descriptionBlock}>ava+desc</div>
         </div>
     );
 };
