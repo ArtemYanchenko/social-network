294cb5d1cd815bbda49afc931369bcb51ef0867b
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-11 00:11:30 +0300

    edit UI

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,6 +6,7 @@ const  Header = () => {
     return (
         <header className={classes.header}>
             <img src="https://cdn-icons-png.flaticon.com/128/124/124019.png" alt="logo"/>
+            <input type="text" placeholder={'Search'}/>
         </header>
     );
 };
--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,7 +4,7 @@ import classes from './ProfileInfo.module.css';
 
 const ProfileInfo = () => {
     return (
-        <div>
+        <div className={classes.profileInfoBlock}>
             <img
                 className={classes.mainImg}
                 src="https://img3.akspic.ru/crops/7/3/3/7/4/147337/147337-macbook-makbuk_ejr-voda-tkan-more-3840x2160.jpg"
