abedf8076e00d280ee7a7a2fda33442b0dd29236
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-06-17 14:45:48 +0300

    done 60 lesson (withRouter)

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
                 <Navbar/>
                 <div className="app-wrapper-content">
                     <Switch>
-                        <Route path={'/profile'} component={ProfileContainer}/>
+                        <Route path={'/profile/:userId'} component={ProfileContainer}/>
                         <Route path={'/dialogs'} component={DialogsContainer}/>
                         <Route path={'/users'} component={UsersContainer}/>
                         <Route path={'/news'} component={News}/>
--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,28 +1,32 @@
-import classes from './Profile.module.css';
-import ProfileInfo from './ProfileInfo/ProfileInfo';
-import MyPostsContainer from './MyPosts/MyPostsContainer';
 import React from 'react';
 import Profile from './Profile';
 import axios from 'axios';
 import {connect} from 'react-redux';
 import {setUserProfile} from '../../redux/profile-reducer';
 import {AppStateType} from '../../redux/redux-store';
+import {withRouter} from 'react-router-dom';
 
 class ProfileContainer extends React.Component<any, any> {
+
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-            .then((res)=>{
+        const userId = this.props.match.params.userId
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
+            .then((res) => {
                 this.props.setUserProfile(res.data)
             })
     }
 
     render() {
-      return <Profile profile={this.props.profile}/>
+        debugger
+        return <Profile profile={this.props.profile}/>
     }
 }
 
-const mapStateToProps = (state:AppStateType) => ({
-    profile:state.profilePage.profile
+const mapStateToProps = (state: AppStateType) => ({
+    profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps,({setUserProfile}))(ProfileContainer)
+
+const ProfileContainterWithRouter = withRouter(ProfileContainer)
+
+export default connect(mapStateToProps, ({setUserProfile}))(ProfileContainterWithRouter)
