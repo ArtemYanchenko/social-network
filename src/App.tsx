1fc595910feed5cf451fe6bc50163ae332141134
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-08-04 20:30:00 +0300

    refactoring app login flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {Redirect, Route, Switch, withRouter} from 'react-router-dom';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+import {withRouter} from 'react-router-dom';
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginContainer from './components/Login/LoginContainer';
 import {connect} from 'react-redux';
@@ -14,7 +9,42 @@ import {AppStateType} from './bll/redux-store';
 import {compose} from 'redux';
 import {initializeApp} from './bll/app-reducer';
 import PreLoader from './components/common/PreLoader';
+import Routing from './dal/Routing';
 
+
+class App extends Component<PropsType> {
+    componentDidMount() {
+        this.props.initialize();
+    }
+
+    render() {
+        if (!this.props.initialized) {
+            return <PreLoader/>
+        }
+
+        return (
+            <div className="App">
+                    <HeaderContainer/>
+                    {this.props.isLoggedIn && <Navbar/>}
+                    <div className="app-wrapper-content">
+                        {this.props.isLoggedIn ? <Routing/> : <LoginContainer/>}
+                    </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
+    initialized: state.app.initialized,
+    isLoggedIn: state.auth.isLoggedIn
+})
+
+export const AppContainer = compose<React.ComponentType>(
+    withRouter,
+    connect(mapStateToProps, {initialize: initializeApp}))(App)
+
+
+//types
 type MapStateToPropsType = {
     initialized: boolean
     isLoggedIn: boolean
@@ -24,45 +54,4 @@ type MapDispatchToPropsType = {
     initialize: () => void
 }
 
-class App extends Component<MapStateToPropsType & MapDispatchToPropsType> {
-    componentDidMount() {
-        this.props.initialize()
-    }
-
-    render() {
-        if (!this.props.initialized) {
-            return <PreLoader/>
-        }
-        if (!this.props.isLoggedIn) {
-            return <LoginContainer/>
-        }
-        return (
-            <div className="App">
-                <div className="app-wrapper">
-                    <HeaderContainer/>
-                    <Navbar/>
-                    <div className="app-wrapper-content">
-                        <Switch>
-                            <Route path={`/profile/:userId?`} component={ProfileContainer}/>
-                            <Route path={'/dialogs'} component={DialogsContainer}/>
-                            <Route path={'/users'} component={UsersContainer}/>
-                            <Route path={'/news'} component={News}/>
-                            <Route path={'/music'} component={Music}/>
-                            <Route path={'/login'} component={LoginContainer}/>
-                        </Switch>
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
-}
-
-let mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
-    initialized: state.app.initialized,
-    isLoggedIn: state.auth.isLoggedIn
-})
-
-export const AppContainer = compose<React.ComponentType>(
-    withRouter,
-    connect(mapStateToProps, {initialize: initializeApp}))(App)
+type PropsType = MapStateToPropsType & MapDispatchToPropsType
---
+++ src/dal/Routing.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import ProfileContainer from '../components/Profile/ProfileContainer';
+import DialogsContainer from '../components/Dialogs/DialogsContainer';
+import UsersContainer from '../components/Users/UsersContainer';
+import News from '../components/News/News';
+import Music from '../components/Music/Music';
+import LoginContainer from '../components/Login/LoginContainer';
+
+const Routing = () => {
+    return (
+        <Switch>
+
+            <Route path={`/`} component={ProfileContainer}/>
+            <Route path={`/profile/:userId?`} component={ProfileContainer}/>
+            <Route path={'/dialogs'} component={DialogsContainer}/>
+            <Route path={'/users'} component={UsersContainer}/>
+            <Route path={'/news'} component={News}/>
+            <Route path={'/music'} component={Music}/>
+            <Route path={'/login'} component={LoginContainer}/>
+        </Switch>
+    );
+};
+
+export default Routing;
